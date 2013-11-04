efae2f0092c2506bc8c06fad09bee0a82525d1c3
beckharvard <beck@fas.harvard.edu>
2013-11-03 20:23:52 -0500

    Changed max size to 72 chars

--- views/v_posts_add.php
+++ views/v_posts_add.php
@@ -1,6 +1,6 @@
 <form method ='POST' action ='/posts/p_add'>
 	Add your post text:<br>
-	<textarea id='myPost' cols="72" rows="25" type='text' name="content" required></textarea>
+	<textarea id='myPost' cols="52" rows="1" type='text' name="content" maxlength="72" required></textarea>
 		<br>
 	<input class="buttons" type='submit' value='Publish Post'>
 </form>
--- views/v_posts_edit.php
+++ views/v_posts_edit.php
@@ -3,7 +3,7 @@
 Edit your post
 	<br>
 	<!-- the next line has to have the PHP right after the "require>" or I get spaces at left in edit post --->
-	<textarea id='myPost' cols="72" rows="25" type='text' name='content' required><?=$post['content'] = trim($post['content'], " \t\n\r" )?>
+	<textarea id='myPost' cols="52" rows="1" type='text' name='content' maxlength="72" required><?=$post['content'] = trim($post['content'], " \t\n\r" )?>
 	</textarea>
     <br>
     <input class="buttons" type='submit' value='Update Post'>
