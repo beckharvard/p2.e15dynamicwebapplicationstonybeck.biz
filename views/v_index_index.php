535dc581771a002e7c1e5f3f2537a287227c5f67
beckharvard <beck@fas.harvard.edu>
2013-12-11 14:25:01 -0500

    Since it's graded, I might as well push the updates.

--- views/v_index_index.php
+++ views/v_index_index.php
@@ -5,11 +5,13 @@
 <?php else: ?>
 	<!-- Send them back to the login page.-->
 	<h2> Welcome to SpecSpec, please Login or Sign up above! </h2>
-	
+	<br>
 	<p>
-		SpecSpec is a microblogging site. YOu can add posts, edit them and share them with other <br/>
+		SpecSpec is a microblogging site. You can add posts, edit them and share them with other <br/>
 		SpecSpec members!
 	</p>
+	<br> 
+	<p> Click on Sign up above and you can start micro-bloggin' with SpecSpec!"
 <?php endif; ?>
 
 
